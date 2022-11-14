d6ee9af95fb13a93d0ce4f6a8e5803946625e53a
Avinesh Pratap Singh <pratapavinesh2003@gmail.com>
2022-11-15 01:37:29 +0530

    database_update

--- Project_info/Database_Info/DDL.sql
+++ Project_info/Database_Info/DDL.sql
@@ -1,87 +1,91 @@
-drop DATABASE if exists HMS;
-CREATE DATABASE HMS;
-USE HMS;
+drop DATABASE if exists HospitalManagementSystem;
+CREATE DATABASE HospitalManagementSystem;
+USE HospitalManagementSystem;
 
-CREATE TABLE Patient(
-email varchar(50) PRIMARY KEY,
-password varchar(30) NOT NULL,
-name varchar(50) NOT NULL,
-address varchar(60) NOT NULL,
-gender VARCHAR(20) NOT NULL
+CREATE TABLE Patient_Information_Table(
+Email_of_Patient varchar(100),
+Name_of_Patient varchar(100) NOT NULL,
+Password_of_Patient varchar(100) NOT NULL,
+Gender_of_Patient VARCHAR(100) NOT NULL,
+Address_of_Patient varchar(100) NOT NULL,
+PRIMARY KEY(Email_of_Patient)
 );
 
-CREATE TABLE MedicalHistory(
-id int PRIMARY KEY,
-date DATE NOT NULL,
-conditions VARCHAR(100) NOT NULL, 
-surgeries VARCHAR(100) NOT NULL, 
-medication VARCHAR(100) NOT NULL
+CREATE TABLE Medical_History_of_Patient(
+Id_of_Medical_History int,
+Date_of_Visit DATE NOT NULL,
+Conditions_of_Patient VARCHAR(1000) NOT NULL, 
+Surgeries_of_Patient VARCHAR(1000) NOT NULL, 
+Medication_of_Patient VARCHAR(1000) NOT NULL,
+PRIMARY KEY(Id_of_Medical_History)
 );
 
-CREATE TABLE Doctor(
-email varchar(50) PRIMARY KEY,
-gender varchar(20) NOT NULL,
-password varchar(30) NOT NULL,
-name varchar(50) NOT NULL
+CREATE TABLE Doctor_Information_Table(
+Email_of_Doctor varchar(100),
+Name_of_Doctor varchar(100) NOT NULL,
+Gender_of_Doctor varchar(100) NOT NULL,
+Password_of_Doctor varchar(100) NOT NULL,
+PRIMARY KEY(Email_of_Doctor)
 );
 
-CREATE TABLE Appointment(
-id int PRIMARY KEY,
-date DATE NOT NULL,
-starttime TIME NOT NULL,
-endtime TIME NOT NULL,
-status varchar(15) NOT NULL
+CREATE TABLE Appointment_Information_Table(
+Id_of_Appointment int,
+Date_of_Appointment DATE NOT NULL,
+Starttime_of_Appointment TIME NOT NULL,
+Endtime_of_Appointment TIME NOT NULL,
+Status_of_Appointment varchar(100) NOT NULL,
+PRIMARY KEY(Id_of_Appointment)
 );
 
-CREATE TABLE PatientsAttendAppointments(
-patient varchar(50) NOT NULL,
-appt int NOT NULL,
-concerns varchar(40) NOT NULL,
-symptoms varchar(40) NOT NULL,
-FOREIGN KEY (patient) REFERENCES Patient (email) ON DELETE CASCADE,
-FOREIGN KEY (appt) REFERENCES Appointment (id) ON DELETE CASCADE,
-PRIMARY KEY (patient, appt)
+CREATE TABLE Patient_Attend_Appointments_Information_Table(
+Id_of_Appointment int NOT NULL,
+Email_of_Patient varchar(100) NOT NULL,
+Concerns_of_Patient varchar(100) NOT NULL,
+Symptoms_of_Patient varchar(100) NOT NULL,
+PRIMARY KEY (Email_of_Patient, Id_of_Appointment),
+FOREIGN KEY (Email_of_Patient) REFERENCES Patient_Information_Table (Email_of_Patient) ON DELETE CASCADE,
+FOREIGN KEY (Id_of_Appointment) REFERENCES Appointment_Information_Table (Id_of_Appointment) ON DELETE CASCADE
 );
 
-CREATE TABLE Schedule(
-id int NOT NULL,
-starttime TIME NOT NULL,
-endtime TIME NOT NULL,
-breaktime TIME NOT NULL,
-day varchar(20) NOT NULL,
-PRIMARY KEY (id, starttime, endtime, breaktime, day)
+CREATE TABLE Schedule_Information_Table(
+Id_of_Schedule int NOT NULL,
+Starttime_of_Schedule TIME NOT NULL,
+Endtime_of_Schedule TIME NOT NULL,
+Breaktime_of_Schedule TIME NOT NULL,
+Day_of_Schedule varchar(100) NOT NULL,
+PRIMARY KEY (Id_of_Schedule, Starttime_of_Schedule, Endtime_of_Schedule, Breaktime_of_Schedule, Day_of_Schedule)
 );
 
-CREATE TABLE PatientsFillHistory(
-patient varchar(50) NOT NULL,
-history int NOT NULL,
-FOREIGN KEY (patient) REFERENCES Patient (email) ON DELETE CASCADE,
-FOREIGN KEY (history) REFERENCES MedicalHistory (id) ON DELETE CASCADE,
-PRIMARY KEY (history)
+CREATE TABLE Patients_Fill_History_Information_Table(
+Email_of_Patient varchar(100) NOT NULL,
+Id_of_Medical_History int NOT NULL,
+PRIMARY KEY (Id_of_Medical_History),
+FOREIGN KEY (Email_of_Patient) REFERENCES Patient_Information_Table(Email_of_Patient) ON DELETE CASCADE,
+FOREIGN KEY (Id_of_Medical_History) REFERENCES Medical_History_of_Patient(Id_of_Medical_History) ON DELETE CASCADE
 );
 
-CREATE TABLE Diagnose(
-appt int NOT NULL,
-doctor varchar(50) NOT NULL,
-diagnosis varchar(40) NOT NULL,
-prescription varchar(50) NOT NULL,
-FOREIGN KEY (appt) REFERENCES Appointment (id) ON DELETE CASCADE,
-FOREIGN KEY (doctor) REFERENCES Doctor (email) ON DELETE CASCADE,
-PRIMARY KEY (appt, doctor)
+CREATE TABLE Diagnose_Information_Table(
+Id_of_Appointment int NOT NULL,
+Email_of_Doctor varchar(100) NOT NULL,
+Diagnosis_of_Patient varchar(100) NOT NULL,
+Prescription_To_Patient varchar(100) NOT NULL,
+PRIMARY KEY (Id_of_Appointment, Email_of_Doctor),
+FOREIGN KEY (Id_of_Appointment) REFERENCES Appointment_Information_Table(Id_of_Appointment) ON DELETE CASCADE,
+FOREIGN KEY (Email_of_Doctor) REFERENCES Doctor_Information_Table(Email_of_Doctor) ON DELETE CASCADE
 );
 
-CREATE TABLE DocsHaveSchedules(
-sched int NOT NULL,
-doctor varchar(50) NOT NULL,
-FOREIGN KEY (sched) REFERENCES Schedule (id) ON DELETE CASCADE,
-FOREIGN KEY (doctor) REFERENCES Doctor (email) ON DELETE CASCADE,
-PRIMARY KEY (sched, doctor)
+CREATE TABLE Doctors_Have_Schedules_Information_Table(
+Id_of_Schedule int NOT NULL,
+Email_of_Doctor varchar(100) NOT NULL,
+PRIMARY KEY (Id_of_Schedule, Email_of_Doctor),
+FOREIGN KEY (Id_of_Schedule) REFERENCES Schedule_Information_Table (Id_of_Schedule) ON DELETE CASCADE,
+FOREIGN KEY (Email_of_Doctor) REFERENCES Doctor_Information_Table (Email_of_Doctor) ON DELETE CASCADE
 );
 
-CREATE TABLE DoctorViewsHistory(
-history int NOT NULL,
-doctor varchar(50) NOT NULL,
-FOREIGN KEY (doctor) REFERENCES Doctor (email) ON DELETE CASCADE,
-FOREIGN KEY (history) REFERENCES MedicalHistory (id) ON DELETE CASCADE,
-PRIMARY KEY (history, doctor)
+CREATE TABLE Doctor_Views_History_Information_Table(
+Email_of_Doctor varchar(100) NOT NULL,
+Id_of_Medical_History int NOT NULL,
+PRIMARY KEY (Id_of_Medical_History, Email_of_Doctor),
+FOREIGN KEY (Email_of_Doctor) REFERENCES Doctor_Information_Table (Email_of_Doctor) ON DELETE CASCADE,
+FOREIGN KEY (Id_of_Medical_History) REFERENCES Medical_History_of_Patient (Id_of_Medical_History) ON DELETE CASCADE
 );
--- Project_info/Database_Info/InsertDML.sql
+++ Project_info/Database_Info/InsertDML.sql
@@ -1,39 +1,33 @@
-INSERT INTO Patient(email,password,name,address,gender)
-VALUES
-('ramesh@gmail.com','avi','Ramesh','Tamil Nadu', 'male'),
-('suresh@gmail.com','avin','Suresh','Karnataka', 'male'),
-('rakesh@gmail.com','avine','Rakesh','Gujarat', 'male')
+INSERT INTO Patient_Information_Table VALUES
+('ritik@gmail.com','ritik','ritik08', 'male','Tamil Nadu'),
+('rashi@gmail.com','rashi','rashikr08', 'male','Karnataka'),
+('avinesh@gmail.com','avinesh','avinesh10','male','Gujarat')
 ;
 
-INSERT INTO MedicalHistory(id,date,conditions,surgeries,medication)
-VALUES
+INSERT INTO Medical_History_of_Patient VALUES
 (1,'19-01-14','Pain in abdomen','Heart Surgery','Crocin'),
 (2,'19-01-14','Frequent Indigestion','none','none'),
 (3,'19-01-14','Body Pain','none','Iodex')
 ;
 
-INSERT INTO Doctor(email, gender, password, name)
-VALUES
-('brijesh7@gmail.com', 'male', 'avi', 'brijesh kumar'),
-('brijesh8@gmail.com', 'male', 'hrishikesh13', 'brijesh kumar2')
+INSERT INTO Doctor_Information_Table VALUES
+('sundramc@gmail.com', 'Sundram','male', 'kjsbdvjhbds'),
+('shravanpanday@gmail.com', 'Shravan','male', 'akjbhjvbskjbse')
 ;
 
-INSERT INTO Appointment(id,date,starttime,endtime,status)
-VALUES
+INSERT INTO Appointment_Information_Table VALUES
 (1, '19-01-15', '09:00', '10:00', 'Done'),
 (2, '19-01-16', '10:00', '11:00', 'Done'),
 (3, '19-01-18', '14:00', '15:00', 'Done')
 ;
 
-INSERT INTO PatientsAttendAppointments(patient,appt,concerns,symptoms)
-VALUES
-('ramesh@gmail.com',1, 'none', 'itchy throat'),
-('suresh@gmail.com',2, 'infection', 'fever'),
-('rakesh@gmail.com',3, 'nausea', 'fever')
+INSERT INTO Patient_Attend_Appointments_Information_Table VALUES
+(1,'ritik@gmail.com', 'none', 'itchy throat'),
+(2,'rashi@gmail.com', 'infection', 'fever'),
+(3,'avinesh@gmail.com','nausea', 'fever')
 ;
 
-INSERT INTO Schedule(id,starttime,endtime,breaktime,day)
-VALUES
+INSERT INTO Schedule_Information_Table VALUES
 (001,'09:00','17:00','12:00','Tuesday'),
 (001,'09:00','17:00','12:00','Friday'),
 (001,'09:00','17:00','12:00','Saturday'),
@@ -42,29 +36,25 @@ VALUES
 (002,'09:00','17:00','12:00','Friday')
 ;
 
-INSERT INTO PatientsFillHistory(patient,history)
-VALUES
-('ramesh@gmail.com', 1),
-('suresh@gmail.com', 2),
-('rakesh@gmail.com', 3)
+INSERT INTO Patients_Fill_History_Information_Table VALUES
+('ritik@gmail.com', 1),
+('rashi@gmail.com', 2),
+('avinesh@gmail.com', 3)
 ;
 
-INSERT INTO Diagnose(appt,doctor,diagnosis,prescription)
-VALUES
-(1,'brijesh7@gmail.com', 'Bloating', 'Ibuprofen as needed'),
-(2,'brijesh8@gmail.com', 'Muscle soreness', 'Stretch morning/night'),
-(3,'brijesh8@gmail.com', 'Vitamin Deficiency', 'Good Diet')
+INSERT INTO Diagnose_Information_Table VALUES
+(1,'sundramc@gmail.com', 'Bloating', 'Ibuprofen as needed'),
+(2,'shravanpanday@gmail.com', 'Muscle soreness', 'Stretch morning/night'),
+(3,'shravanpanday@gmail.com', 'Vitamin Deficiency', 'Good Diet')
 ;
 
-INSERT INTO DocsHaveSchedules(sched,doctor)
-VALUES
-(001,'brijesh7@gmail.com'),
-(002,'brijesh8@gmail.com')
+INSERT INTO Doctors_Have_Schedules_Information_Table VALUES
+(001,'sundramc@gmail.com'),
+(002,'shravanpanday@gmail.com')
 ;
 
-INSERT INTO DoctorViewsHistory(history,doctor)
-VALUES
-(1,'brijesh7@gmail.com'),
-(2,'brijesh8@gmail.com'),
-(3,'brijesh8@gmail.com')
+INSERT INTO Doctor_Views_History_Information_Table VALUES
+('sundramc@gmail.com',1),
+('shravanpanday@gmail.com',2),
+('shravanpanday@gmail.com',3)
 ;
